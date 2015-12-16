aca8f9604c4230711cdfe860c7641f989149dc02
philiptang42 <philiptang42@gmail.com>
2015-12-15 23:03:31 -0500

    solution:

--- student_ranking.rb
+++ student_ranking.rb
@@ -1,3 +1,9 @@
 def ranking(students)
-  
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student}"
+  end
 end
+
+geometry_kids = ['Johnny', 'Jane', 'Sally', 'Elizabeth', 'Michael']
+
+ranking(geometry_kids)
